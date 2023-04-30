660afdde5deb221a174bc5f05bc8906e8b875527
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-04-30 19:38:25 +0800

    refactored authService.validate method

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -4,32 +4,31 @@ import { ErrorMessage, SignUpDto } from './auth.dto';
 @Injectable()
 export class AuthService {
   
-  errorMessage = {
-    email: '',
-    username: '',
-    password: '',
-  }
-
   validate(userData: SignUpDto): [boolean, ErrorMessage] {
+    let errorMessage: ErrorMessage = {
+      email: '',
+      username: '',
+      password: '',
+    }
     const { email, username, password, confirmPassword } = userData;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
     const isValidEmail = emailRegex.test(email);
 
-    if (!email) this.errorMessage.email = 'Email field is required'
-      else this.errorMessage.email = '';
-    if (!isValidEmail) this.errorMessage.email = 'Email field is not a valid email';
-      else this.errorMessage.email = '';
-    if (!username) this.errorMessage.username = 'Username field is required';
-      else this.errorMessage.username = '';
-    if (!password) this.errorMessage.password = 'Password field is required';
-      else this.errorMessage.password = '';
-    if (password.length < 10) this.errorMessage.password = 'Password length too short';
-      else this.errorMessage.password = '';
-    if (password !== confirmPassword) this.errorMessage.password = "Password doesn't match";
-      else this.errorMessage.password = '';
+    if (!email) errorMessage.email = 'Email field is required'
+      else errorMessage.email = '';
+    if (!isValidEmail) errorMessage.email = 'Email field is not a valid email';
+      else errorMessage.email = '';
+    if (!username) errorMessage.username = 'Username field is required';
+      else errorMessage.username = '';
+    if (!password) errorMessage.password = 'Password field is required';
+      else errorMessage.password = '';
+    if (password.length < 10) errorMessage.password = 'Password length too short';
+      else errorMessage.password = '';
+    if (password !== confirmPassword) errorMessage.password = "Password doesn't match";
+      else errorMessage.password = '';
 
-    return this.errorMessage.email || this.errorMessage.username || this.errorMessage.password
-      ? [false, this.errorMessage]
+    return errorMessage.email || errorMessage.username || errorMessage.password
+      ? [false, errorMessage]
       : [true, null];
   }
 
