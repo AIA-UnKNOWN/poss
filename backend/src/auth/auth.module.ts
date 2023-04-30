497d775534d0cd4c2768a4299dc640a9e48a6be9
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-04-30 18:06:31 +0800

    refactored users and auth module for import/exports stuff

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { User } from 'src/users/entity/user.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthService, UsersService],
+  imports: [UsersModule],
+  providers: [AuthService],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -6,6 +6,7 @@ import { User } from './entity/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  exports: [UsersService],
   providers: [UsersService],
   controllers: [UsersController],
 })
