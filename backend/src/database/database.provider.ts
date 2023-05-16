6d508d6e53b62e1ead9035b81fa316f5dc722678
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-05-16 00:10:13 +0000

    setup typeorm migration cli

--- backend/src/database/database.provider.ts
+++ backend/src/database/database.provider.ts
@@ -1,11 +1,4 @@
 import { DataSource } from 'typeorm';
-import { User } from 'src/modules/users/entity/user.entity';
-import { Product } from 'src/modules/products/entity/product.entity';
-import { Category } from 'src/modules/categories/entity/category.entity';
-import { Photo } from 'src/modules/photos/entity/photos.entity';
-import { Order } from 'src/modules/orders/entity/order.entity';
-import { Transaction } from 'src/modules/transactions/entity/transaction.entity';
-import { Role } from 'src/modules/roles/entity/role.entity';
 
 const dataSource = new DataSource({
   type: 'mysql',
@@ -14,15 +7,8 @@ const dataSource = new DataSource({
   username: 'root',
   password: 'AjboyIan_321',
   database: 'poss',
-  entities: [
-    User,
-    Product,
-    Category,
-    Photo,
-    Order,
-    Transaction,
-    Role,
-  ],
+  entities: ['dist/modules/**/*.entity.js'],
+  migrations: ['dist/database/migrations/*.js'],
   synchronize: true,
 });
 
