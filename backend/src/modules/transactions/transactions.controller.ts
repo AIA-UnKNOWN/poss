5f9aebb96f81481da1ccabf61f4660356092e1be
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-05-28 01:24:09 +0000

    added auth guard for transactions endpoint

--- backend/src/modules/transactions/transactions.controller.ts
+++ backend/src/modules/transactions/transactions.controller.ts
@@ -1,4 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('transactions')
 export class TransactionsController {}
