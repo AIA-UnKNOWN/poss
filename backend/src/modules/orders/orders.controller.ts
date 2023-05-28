361e9bcb1ae0a57f4870e0d5643569051406b23a
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-05-28 01:26:01 +0000

    added auth guard for orders endpoints

--- backend/src/modules/orders/orders.controller.ts
+++ backend/src/modules/orders/orders.controller.ts
@@ -1,4 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('orders')
+@UseGuards(AuthGuard)
 export class OrdersController {}
