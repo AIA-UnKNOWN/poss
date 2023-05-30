35b19adb6136e439bec34d7aa63a334fc8b4d20b
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-05-30 00:11:27 +0000

    added icon options for buttons

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,17 +1,32 @@
 import './Button.style.scss';
 import React from 'react';
-import { AiFillCheckCircle } from "react-icons/ai";
+// Icons
+import { FaAngleLeft } from "react-icons/fa";
 // Types
 import type { Button } from './Button.types';
 
 const Button: React.FC<Button> = props => {
-  const { text } = props;
+  const { text, iconName } = props;
+
+  const renderIcon = () : JSX.Element | null => {
+    let icon = null;
+    switch(iconName) {
+      case 'angle-left':
+        icon = <FaAngleLeft />;
+        break;
+      default:
+        icon = null;
+    }
+    return icon === null ? null : (
+      <div className='icon-container'>
+        {icon}
+      </div>
+    );
+  }
 
   return (
     <button className='primary-button'>
-      <div className='icon-container'>
-        <AiFillCheckCircle />
-      </div>
+      {renderIcon()}
       {text}
     </button>
   )
