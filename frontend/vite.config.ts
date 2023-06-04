09030176154a4c2a30949311a65f9db35d38118e
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-04 19:07:01 +0800

    added module alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { host: '0.0.0.0' },
+  resolve: {
+    alias: {
+      'src': path.join(__dirname, 'src'),
+    }
+  }
 })
