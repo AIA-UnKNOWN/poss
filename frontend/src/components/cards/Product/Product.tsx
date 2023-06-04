ee1c83071c0808cc3c33004d4904ea754cd2d0d7
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-04 11:17:42 +0800

    created another product variant callled order-item

--- frontend/src/components/cards/Product/Product.tsx
+++ frontend/src/components/cards/Product/Product.tsx
@@ -1,17 +1,58 @@
 import './Product.style.scss';
 import React from "react";
 import type { ProductProps } from "./Product.types";
+import Button from '../../Button/Button';
 
 const Product: React.FC<ProductProps> = props => {
-  const { product, view } = props;
+  const {
+    product,
+    view = 'default',
+  } = props;
   const {
     name,
     price,
     photoUrl,
+    quantity,
   } = product;
 
   const renderProduct = () : JSX.Element => {
     switch(view) {
+      case 'order-item':
+        return (
+          <div className="product order-item">
+            <div className='order-item-details'>
+              <div className='product-image-container order-item'>
+                {photoUrl && (
+                  <img
+                    src={photoUrl}
+                    alt={name}
+                  />
+                )}
+              </div>
+              <div className='product-details order-item'>
+                <span className='product-name order-item'>{name}</span>
+                <span className='product-price order-item'>${price}</span>
+              </div>
+            </div>
+            <div className='order-item-quantity-control'>
+              <Button
+                text='increment'
+                showText={false}
+                showLeftIcon={true}
+                iconName='plus'
+                size='sm'
+              />
+              <span>{quantity}</span>
+              <Button
+                text='decrement'
+                showText={false}
+                showLeftIcon={true}
+                iconName='minus'
+                size='sm'
+              />
+            </div>
+          </div>
+        );
       default:
         return (
           <div className="product">
--- frontend/src/components/cards/Product/Product.types.ts
+++ frontend/src/components/cards/Product/Product.types.ts
@@ -10,5 +10,5 @@ export type Product = {
 
 export interface ProductProps {
   product: Product;
-  view?: string;
+  view?: 'default' | 'order-item';
 }
