de146d5b34c8e1195ffcbcadaf9bfc40e6417aaa
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-10 18:51:20 +0800

    added devtool logging for navigation state

--- frontend/src/store/navigation.ts
+++ frontend/src/store/navigation.ts
@@ -1,13 +1,16 @@
 import { create } from "zustand";
+import { devtools } from 'zustand/middleware'
 
 export interface Navigation {
   pageName: string;
   navigateToPage: (pageName: string) => void,
 }
 
-const useNavigationStore = create<Navigation>((set) => ({
-  pageName: '',
-  navigateToPage: pageName => set(state => ({ pageName })),
-}));
+const useNavigationStore = create<Navigation>()(
+  devtools((set) => ({
+    pageName: '',
+    navigateToPage: pageName => set(state => ({ pageName })),
+  }))
+);
 
 export default useNavigationStore;
