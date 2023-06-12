2dcda0c44baaebfb0f6fe4401477d91521f735ce
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-12 21:06:37 +0800

    configured routing for the app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
 import './_index.style.scss';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/signup",
+    element: <h1>SignUp Page</h1>
+  },
+  {
+    path: "/signin",
+    element: <h1>SignIn Page</h1>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
