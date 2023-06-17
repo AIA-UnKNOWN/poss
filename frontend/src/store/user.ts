345c4b8c2086d8ed5bc231e4d2ec5981259731ce
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-17 15:56:29 +0800

    refactored user store

--- frontend/src/store/user.ts
+++ frontend/src/store/user.ts
@@ -1,6 +1,5 @@
 import { create } from "zustand";
 import { devtools } from 'zustand/middleware';
-import { setCookie } from 'typescript-cookie';
 
 export type User = {
   id: number;
@@ -10,35 +9,12 @@ export type User = {
 }
 
 export type UserState = {
-  user: User | null;
-  isAuthenticated: null | boolean;
-  signIn: (details: { username: string, password: string }) => void;
+  data?: User;
 }
 
 const useUserStore = create<UserState>()(
   devtools(set => ({
-    user: null,
-    isAuthenticated: null,
-    signIn: async ({ username, password }) => {
-      try {
-        const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/auth/signIn`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username, password }),
-        });
-        const { accessToken, statusCode } = await response.json();
-        set(() => {
-          if (statusCode === 401) return { isAuthenticated: false };
-          setCookie('ut', accessToken);
-          location.href = '/';
-          return { isAuthenticated: true, token: accessToken };
-        });
-      } catch(error) {
-        console.error(error);
-      }
-    },
+    data: undefined,
   }))
 );
 
