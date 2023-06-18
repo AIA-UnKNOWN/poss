4422880655eb85afab8b935b1381999a7f6b72b5
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-18 17:20:51 +0800

    added authservice.signup api

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -7,10 +7,22 @@ export type SignInData = {
   password: string;
 }
 
+export type SignUpData = SignInData & {
+  email: string;
+  confirmPassword: string;
+}
+
 const authService = {
   signIn: (data: SignInData) => new Promise(
     (resolve, reject) => {
       axios.post(`${endpoint}/signIn`, data)
+        .then(response => resolve(response.data))
+        .catch(error => reject(error));
+    }
+  ),
+  signUp: (data: SignUpData) => new Promise(
+    (resolve, reject) => {
+      axios.post(`${endpoint}/signUp`, data)
         .then(response => resolve(response.data))
         .catch(error => reject(error));
     }
