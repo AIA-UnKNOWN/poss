f2bed133aa9c2fc8895753b635640813997d276f
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-19 20:43:24 +0800

    created an endpoint that gets the current user based on the current client token

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,19 +1,26 @@
 import {
   Controller,
   Post,
+  Get,
   Body,
+  Req,
   HttpCode,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
 import { SignInDto } from './auth.dto';
 import { CreateUserDto } from '../users/user.dto';
+import { AuthGuard } from './auth.guard';
+import { UsersService } from 'src/modules/users/users.service';
 
 @Controller('auth')
 export class AuthController {
 
   constructor(
     private readonly authService: AuthService,
+    private readonly userService: UsersService,
   ) {}
 
   @Post('signup')
@@ -25,6 +32,13 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   signIn(@Body() userData: SignInDto) {
     return this.authService.signIn(userData);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('user')
+  getCurrentUser(@Req() request: Request) {
+    const userId = request['user'].sub;
+    return this.userService.findOne(userId);
   }
 
 }
