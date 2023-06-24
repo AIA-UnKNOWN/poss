06a0ec00fc66a2a7b292907a61f650b13bde4f8e
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-24 08:57:56 +0800

    integrated sample modal in inventory page

--- frontend/src/views/Inventory/Inventory.hook.ts
+++ frontend/src/views/Inventory/Inventory.hook.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // Store
 import useProductStore from "src/store/products";
 import useCategoryStore from "src/store/categories";
@@ -6,15 +6,22 @@ import useCategoryStore from "src/store/categories";
 const useInventory = () => {
   const productStore = useProductStore();
   const categoryStore = useCategoryStore();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     productStore.getAll();
     categoryStore.getAll();
   }, []);
 
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  }
+
   return {
     products: productStore.data,
     categories: categoryStore.data,
+    isModalOpen,
+    toggleModal,
   }
 }
 
--- frontend/src/views/Inventory/Inventory.tsx
+++ frontend/src/views/Inventory/Inventory.tsx
@@ -7,57 +7,71 @@ import Input from "src/components/Input/Input";
 import Button from "src/components/Button";
 import Product from "src/components/cards/Product/Product";
 import EmptyState from 'src/components/cards/EmptyState/EmptyState';
+import Modal from 'src/components/Modal';
 
 const Inventory = () => {
   const {
     products,
     categories,
+    isModalOpen,
+    toggleModal,
   } = useInventory();
 
   return (
-    <div className="inventory">
-      <Input
-        placeholder="Search product name, code, or category"
-        onChange={({ target: { value } }) => console.log(value)}
-      />
-      <div className="inventory-categories-container">
-        <div className="actions-container">
-          <Button
-            text="Add category"
-          />
-        </div>
-        <div className='inventory-categories'>
-          {categories.length > 0 ? categories.map(category => (
-            <button key={category.id} className='category'>
-              {category.name}
-            </button>
-          )) : (
-            <EmptyState
-              text='No categories available.'
+    <>
+      <Modal
+        isOpen={isModalOpen}
+        onCloseModal={toggleModal}
+      >
+        <h1>This is modal</h1>
+      </Modal>
+      
+      <div className="inventory">
+        <Input
+          placeholder="Search product name, code, or category"
+          onChange={({ target: { value } }) => console.log(value)}
+        />
+        <div className="inventory-categories-container">
+          <div className="actions-container">
+            <Button
+              text="Add category"
+              onClick={toggleModal}
             />
-          )}
+          </div>
+          <div className='inventory-categories'>
+            {categories.length > 0 ? categories.map(category => (
+              <button key={category.id} className='category'>
+                {category.name}
+              </button>
+            )) : (
+              <EmptyState
+                text='No categories available.'
+              />
+            )}
+          </div>
+        </div>
+        <div className="inventory-products-container">
+          <div className="actions-container">
+            <Button
+              text="Add product"
+              onClick={toggleModal}
+            />
+          </div>
+          <div className="inventory-products">
+            {products.length > 0 ? products.map(product => (
+              <Product
+                key={product.id}
+                product={product}
+              />
+            )) : (
+              <EmptyState
+                text='No products available.'
+              />
+            )}
+          </div>
         </div>
       </div>
-      <div className="inventory-products-container">
-        <div className="actions-container">
-          <Button
-            text="Add product"
-          />
-        </div>
-        <div className="inventory-products">
-          {products.length > 0 ? products.map(product => (
-            <Product
-              key={product.id}
-              product={product}
-            />
-          )) : (
-            <EmptyState
-              text='No products available.'
-            />
-          )}
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
