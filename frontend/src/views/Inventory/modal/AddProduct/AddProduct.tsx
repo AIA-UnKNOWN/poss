32d1fc40844482f7179d45deb167889675c09160
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-06-25 15:09:49 +0800

    created and styles Add Product modal

--- frontend/src/views/Inventory/modal/AddProduct/AddProduct.tsx
+++ frontend/src/views/Inventory/modal/AddProduct/AddProduct.tsx
@@ -1,9 +1,44 @@
+import './AddProduct.styles.scss';
+// Components
+import Button from "src/components/Button/Button";
+import Input from "src/components/Input/Input";
+import TextArea from 'src/components/TextArea/TextArea';
+
 const AddProduct = () => {
 
   return (
-    <>
-      <h1>Add Products Modal</h1>
-    </>
+    <div className='modal-content'>
+      <p className='modal-title'>Add Product</p>
+      <div className="multi-input-container">
+        <Input
+          placeholder="Product name"
+          onChange={e => console.log(e.target.value)}
+        />
+        <Input
+          placeholder="Category name"
+          onChange={e => console.log(e.target.value)}
+        />
+      </div>
+      <div className="multi-input-container">
+        <Input
+          placeholder="Quantity"
+          type='number'
+          onChange={e => console.log(e.target.value)}
+        />
+        <Input
+          placeholder="Price"
+          type='number'
+          onChange={e => console.log(e.target.value)}
+        />
+      </div>
+      <TextArea
+        placeholder='Description'
+      />
+      <Button
+        text="Add"
+        onClick={() => console.log('add product')}
+      />
+    </div>
   )
 }
 
