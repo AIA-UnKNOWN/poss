96fef4ffbdb7493075c7dbd5a78bf54aa9cad6ec
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-07-06 13:51:51 +0800

    created empty state ui for order cart items

--- frontend/src/components/layouts/OrderDetailsBar/OrderDetailsBar.tsx
+++ frontend/src/components/layouts/OrderDetailsBar/OrderDetailsBar.tsx
@@ -5,6 +5,7 @@ import type { OrderDetailsBarProps } from './OrderDetailsBar.types';
 import Product from 'src/components/cards/Product';
 import Button from 'src/components/Button';
 import SalesInfo from 'src/components/cards/SalesInfo/SalesInfo';
+import EmptyState from 'src/components/cards/EmptyState';
 
 
 const OrderDetailsBar: React.FC<OrderDetailsBarProps> = props => {
@@ -18,15 +19,22 @@ const OrderDetailsBar: React.FC<OrderDetailsBarProps> = props => {
     <div className='order-details-bar'>
       <div className='order-items-container'>
         <span className='label'>Order Cart</span>
-        <div className='order-items'>
-          {products?.map((product, i) => (
-            <Product
-              key={i}
-              view='order-item'
-              product={product}
-            />
-          ))}
-        </div>
+        {products.length > 0 ? (
+          <div className='order-items'>
+            {products?.map((product, i) => (
+              <Product
+                key={i}
+                view='order-item'
+                product={product}
+              />
+            ))}
+          </div>
+        ) : (
+          <EmptyState
+            text='No items in your cart.'
+            className='order-cart-items-empty-state'
+          />
+        )}
       </div>
       <div className='order-amount-info'>
         <div className='sales-info-container'>
