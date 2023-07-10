d11741d144fcf9c9b167e693a303c2e4c2cb55db
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-07-10 20:41:35 +0800

    create order cart actions UI

--- frontend/src/components/layouts/OrderDetailsBar/OrderDetailsBar.tsx
+++ frontend/src/components/layouts/OrderDetailsBar/OrderDetailsBar.tsx
@@ -1,11 +1,13 @@
 import './OrderDetailsBar.style.scss';
 import React from 'react';
-
+// Types
 import type { OrderDetailsBarProps } from './OrderDetailsBar.types';
+// Components
 import Product from 'src/components/cards/Product';
 import Button from 'src/components/Button';
 import SalesInfo from 'src/components/cards/SalesInfo/SalesInfo';
 import EmptyState from 'src/components/cards/EmptyState';
+import Input from 'src/components/Input/Input';
 
 
 const OrderDetailsBar: React.FC<OrderDetailsBarProps> = props => {
@@ -21,6 +23,17 @@ const OrderDetailsBar: React.FC<OrderDetailsBarProps> = props => {
         <span className='label'>Order Cart</span>
         {products.length > 0 ? (
           <div className='order-items'>
+            <div className='order-cart-actions-container'>
+              <Input
+                type='checkbox'
+              />
+              <div className='order-cart-actions'>
+                <Button
+                  text='Remove'
+                  size='sm'
+                />
+              </div>
+            </div>
             {products?.map((product, i) => (
               <Product
                 key={i}
