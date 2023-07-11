9ee0a1013465d25eee6969a6ed080ab87253214d
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-07-11 13:12:30 +0800

    refactored OrderDetailsBar hook by prettier

--- frontend/src/components/layouts/OrderDetailsBar/OrderDetailsBar.hook.ts
+++ frontend/src/components/layouts/OrderDetailsBar/OrderDetailsBar.hook.ts
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 // Utils
-import { ORDER_CART_ITEMS_KEY, getOrderCartItems } from "src/utils/orderCart.helper";
+import {
+  ORDER_CART_ITEMS_KEY,
+  getOrderCartItems,
+} from "src/utils/orderCart.helper";
 // Store
 import useProductStore from "src/store/products";
 // Types
@@ -8,7 +11,9 @@ import { Product } from "src/components/cards/Product/Product.types";
 
 const useOrderDetailsBar = () => {
   const productStore = useProductStore();
-  const [selectedOrderCartItems, setSelectedOrderCartItems] = useState<Product[]>([]);
+  const [selectedOrderCartItems, setSelectedOrderCartItems] = useState<
+    Product[]
+  >([]);
 
   useEffect(() => {
     getSelectedProducts();
@@ -17,49 +22,50 @@ const useOrderDetailsBar = () => {
   const getSelectedProducts = () => {
     const orderCartItems = productStore.orderCartItems;
     setSelectedOrderCartItems(
-      orderCartItems.filter(
-        orderCartItem => orderCartItem.isSelected
-      )
+      orderCartItems.filter((orderCartItem) => orderCartItem.isSelected)
     );
-  }
+  };
 
   const selectAllOrderCartItems = (isChecked: boolean) => {
     const orderCartItems = getOrderCartItems();
-    const selectedOrderCartItems = orderCartItems.map(orderCartItem => ({
+    const selectedOrderCartItems = orderCartItems.map((orderCartItem) => ({
       ...orderCartItem,
       isSelected: isChecked,
     }));
     setSelectedOrderCartItems(selectedOrderCartItems);
     productStore.setOrderCartItems(selectedOrderCartItems);
-  }
+  };
 
   const removeSelectedOrderCartItems = () => {
     const selectedOrderCartItemIds = selectedOrderCartItems.map(
-      selectedOrderCartItem => selectedOrderCartItem.id
+      (selectedOrderCartItem) => selectedOrderCartItem.id
     );
     // Remove items from localStorage
     const localStorageOrderCartItems = getOrderCartItems();
     localStorage.setItem(
       ORDER_CART_ITEMS_KEY,
-      JSON.stringify(localStorageOrderCartItems.filter(
-        orderCartItem => !selectedOrderCartItemIds.includes(orderCartItem.id)
-      ))
+      JSON.stringify(
+        localStorageOrderCartItems.filter(
+          (orderCartItem) =>
+            !selectedOrderCartItemIds.includes(orderCartItem.id)
+        )
+      )
     );
     // Remove items from global state
     const orderCartItems = productStore.orderCartItems;
     productStore.setOrderCartItems(
       orderCartItems.filter(
-        orderCartItem => !selectedOrderCartItemIds.includes(orderCartItem.id)
+        (orderCartItem) => !selectedOrderCartItemIds.includes(orderCartItem.id)
       )
     );
-  }
+  };
 
   return {
     selectedOrderCartItems,
     // Functions
     selectAllOrderCartItems,
     removeSelectedOrderCartItems,
-  }
-}
+  };
+};
 
 export default useOrderDetailsBar;
