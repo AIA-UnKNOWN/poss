c39817d432ca81d3c364824af1f9e52e2b5e477a
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-07-12 15:35:29 +0800

    created orders findAll endpoint

--- backend/src/modules/orders/orders.controller.ts
+++ backend/src/modules/orders/orders.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
+import { OrdersService } from './orders.service';
 
 @Controller('orders')
 @UseGuards(AuthGuard)
-export class OrdersController {}
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  findAll() {
+    return this.ordersService.findAll();
+  }
+}
--- backend/src/modules/orders/orders.service.ts
+++ backend/src/modules/orders/orders.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ORDER_REPOSITORY } from './orders.provider';
+import { Repository } from 'typeorm';
+import { Order } from './entity/order.entity';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(
+    @Inject(ORDER_REPOSITORY)
+    private ordersRepository: Repository<Order>,
+  ) {}
+
+  findAll(): Promise<Order[]> {
+    return this.ordersRepository.find();
+  }
+}
