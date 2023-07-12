58ad826f28032ee5491416b2172db7084029bc06
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-07-12 15:35:08 +0800

    created transactions findAll endpoint

--- backend/src/modules/transactions/transactions.controller.ts
+++ backend/src/modules/transactions/transactions.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
+import { TransactionsService } from './transactions.service';
 
 @UseGuards(AuthGuard)
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+  constructor(private readonly transactionsService: TransactionsService) {}
+
+  @Get()
+  findAll() {
+    return this.transactionsService.findAll();
+  }
+}
--- backend/src/modules/transactions/transactions.service.ts
+++ backend/src/modules/transactions/transactions.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Inject, Injectable } from '@nestjs/common';
+import { Transaction } from './entity/transaction.entity';
+import { TRANSACTION_REPOSITORY } from './transactions.provider';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+  constructor(
+    @Inject(TRANSACTION_REPOSITORY)
+    private transactionsRepository: Repository<Transaction>,
+  ) {}
+
+  @Get()
+  findAll(): Promise<Transaction[]> {
+    return this.transactionsRepository.find();
+  }
+}
