0082ac3040a8ca1f8b7b57d85197f8532ec04d1f
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-07-12 18:57:05 +0800

    added 'amountReceived' and 'amountChange' column in transaction entity

--- backend/src/modules/transactions/entity/transaction.entity.ts
+++ backend/src/modules/transactions/entity/transaction.entity.ts
@@ -1,23 +1,22 @@
-import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
-import { BaseEntity } from "src/helpers/entity.helper";
-import { User } from "src/modules/users/entity/user.entity";
-import { Order } from "src/modules/orders/entity/order.entity";
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { BaseEntity } from 'src/helpers/entity.helper';
+import { User } from 'src/modules/users/entity/user.entity';
+import { Order } from 'src/modules/orders/entity/order.entity';
 
 @Entity('transactions')
 export class Transaction extends BaseEntity {
   @Column({ default: 0 })
   totalAmount: number;
 
-  @ManyToOne(
-    () => User,
-    user => user.transactions,
-    { cascade: true }
-  )
+  @Column({ nullable: false })
+  amountReceived: number;
+
+  @Column({ default: 0 })
+  amountChange: number;
+
+  @ManyToOne(() => User, (user) => user.transactions, { cascade: true })
   user: User;
 
-  @OneToMany(
-    () => Order,
-    order => order.transaction,
-  )
+  @OneToMany(() => Order, (order) => order.transaction)
   orders: Order[];
 }
