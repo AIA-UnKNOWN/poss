e7f3dc899ec8ada01cbd5f5640a9850b860cfa38
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-07-12 18:54:26 +0800

    added quantity column in order entity

--- backend/src/modules/orders/entity/order.entity.ts
+++ backend/src/modules/orders/entity/order.entity.ts
@@ -1,21 +1,20 @@
-import { Entity, ManyToOne } from "typeorm";
-import { BaseEntity } from "src/helpers/entity.helper";
-import { Product } from "src/modules/products/entity/product.entity";
-import { Transaction } from "src/modules/transactions/entity/transaction.entity";
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BaseEntity } from 'src/helpers/entity.helper';
+import { Product } from 'src/modules/products/entity/product.entity';
+import { Transaction } from 'src/modules/transactions/entity/transaction.entity';
 
 @Entity('orders')
 export class Order extends BaseEntity {
-  @ManyToOne(
-    () => Product,
-    product => product.orders,
-    { onDelete: 'SET NULL' }
-  )
+  @ManyToOne(() => Product, (product) => product.orders, {
+    onDelete: 'SET NULL',
+  })
   product: Product;
 
-  @ManyToOne(
-    () => Transaction,
-    transaction => transaction.orders,
-    { cascade: true }
-  )
+  @ManyToOne(() => Transaction, (transaction) => transaction.orders, {
+    cascade: true,
+  })
   transaction: Transaction;
+
+  @Column({ nullable: false })
+  quantiy: number;
 }
